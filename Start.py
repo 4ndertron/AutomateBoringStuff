a545d57104a08f7d11e3be9cf4fcba6ac7bc2f67
4ndertron <robert.anderson@vivintsolar.com>
2019-04-19 14:07:36 -0600

    Began setting up the environment for the filings mailmerge project using automate the boring stuff as the primary reference source.

---
+++ Start.py
@@ -0,0 +1,8 @@
+"""
+ TODO: ABS: docx management
+ TODO: PythonQuickstart: Setup Sheets API
+ TODO: ABS: Setup app password for email account
+       OR- Push the converted pdf document to the drive so GAS can work it's magic.
+"""
+
+print('Go, fight, win!')
